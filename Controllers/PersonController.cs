c23388c6185791555e296db80dc7a3868a6b4020
Peter Nøbbe <pno@system-metode.dk>
2022-11-22 17:37:47 +0100

    first commit

---
+++ Controllers/PersonController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using simpleRestApi.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace simpleRestApi.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class PersonController : ControllerBase
+	{
+		// GET: api/<PersonController>
+		[HttpGet]
+		public IEnumerable<Person> Get()
+		{
+			List<Person> list = new List<Person>()
+			{
+				new Person()
+				{
+					Name = "Peter",
+					Age = 40,
+					Sex = "Male"
+				}
+			};
+
+			return list;
+		}
+
+		// GET api/<PersonController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			return "value";
+		}
+
+		// POST api/<PersonController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+		}
+
+		// PUT api/<PersonController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<PersonController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
---
+++ Model/Person.cs
@@ -0,0 +1,9 @@
+﻿namespace simpleRestApi.Model
+{
+	public class Person
+	{
+		public string? Name { get; set; }
+		public int Age { get; set; }
+		public string? Sex { get; set; }
+	}
+}
